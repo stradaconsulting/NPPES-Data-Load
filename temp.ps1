c63054ed3e6475379ef1d9ef702c55f0170f08cd
Santiago Oleas <soleas@gmail.com>
2022-05-27 12:01:51 -0400

    Delete temp.ps1
    remove temp.ps1 used for debugging

--- temp.ps1
+++
@@ -1,6 +0,0 @@
-
-# URI for the Taxonomy codes - likely to change
-$txcodesURI = "http://www.nucc.org/images/stories/CSV/nucc_taxonomy_200.csv"
-
-# Step 1 - Download the necessary data
-./Get-NPIData.ps1 "May" "2022" $txcodesURI
