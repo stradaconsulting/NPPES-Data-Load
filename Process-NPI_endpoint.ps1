4d493d7ce8182ad873512125d867c9acec9fbc82
Santiago Oleas <soleas@gmail.com>
2022-05-31 14:20:34 -0400

    Standardize error.dat file naming
    Name endpoint bcp processing error file with error_* prefix so that all error files are grouped together alphabetically

--- Process-NPI_endpoint.ps1
+++ Process-NPI_endpoint.ps1
@@ -19,5 +19,5 @@ $pswd   = $sqlparms['password']
 python CleanCSV.py .\data\$npifilename ".\data\tmp_endpoint.dat" $filtercol $filterval
 
 Write-Host "Loading npidata into SQL Server using bcp..."
-bcp NPPES.dbo.npi_endpoint IN .\data\tmp_endpoint.dat -f npi_endpoint_format.xml -e endpoint_error.dat -m 10 -S $server -T #-U $user -P $pswd 
+bcp NPPES.dbo.npi_endpoint IN .\data\tmp_endpoint.dat -f npi_endpoint_format.xml -e error_endpoint.dat -m 10 -S $server -T #-U $user -P $pswd 
 
