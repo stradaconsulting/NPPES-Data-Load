843a27377e04186d390ff5245d83f24eaa354dd4
Santiago Oleas <soleas@gmail.com>
2022-05-31 17:23:56 -0400

    Update Process-TaxonomyCodes.ps1
    Pointed all data file references to new .\data\ folder. Error file was also renamed to have context and not keep generic error.dat name. Now is called error_taxonomy.dat.  Changed SQL Server authentication to -T (Trusted Connection) since this environment uses Windows Authentication and not Username/Password.

--- Process-TaxonomyCodes.ps1
+++ Process-TaxonomyCodes.ps1
@@ -7,6 +7,6 @@ $user     = $sqlparms['userid']
 $pswd     = $sqlparms['password']
 $server   = $sqlparms['server']
 
-python CleanCSV.py $txfilename "tmp_txcodes.dat"
+python CleanCSV.py .\data\$txfilename ".\data\tmp_txcodes.dat"
 
-bcp NPPES.dbo.txcodes IN .\tmp_txcodes.dat -f tx_format.xml -e error.dat -m 10 -U $user -P $pswd -S $server 
+bcp NPPES.dbo.txcodes IN .\data\tmp_txcodes.dat -f tx_format.xml -e error_taxonomy.dat -m 10 -S $server -T #-U $user -P $pswd 
