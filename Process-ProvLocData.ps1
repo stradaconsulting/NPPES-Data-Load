b27d9b44b381bb37b81720cfe6b141b2d95e3732
Santiago Oleas <soleas@gmail.com>
2022-06-01 08:43:36 -0400

    Update Process-ProvLocData.ps1
    Modified script to look for data files in ./data/ subfolder

--- Process-ProvLocData.ps1
+++ Process-ProvLocData.ps1
@@ -1,8 +1,8 @@
 #Requires -Version 7.0
 #TODO: make filter optional in this script
 Param ( [Parameter(Mandatory=$True)] [ValidateNotNull()] [string] $plfilename, `
-        [Parameter(Mandatory=$True)] [ValidateNotNull()] [string] $filtercol, `
-        [Parameter(Mandatory=$True)] [ValidateNotNull()] [string] $filterval `
+        [Parameter(Mandatory=$False)] [ValidateNotNull()] [string] $filtercol, `
+        [Parameter(Mandatory=$False)] [ValidateNotNull()] [string] $filterval `
        )
 
 # Get SQL credentials
@@ -11,8 +11,8 @@ $server = $sqlparms['server']
 $user   = $sqlparms['userid']
 $pswd   = $sqlparms['password']
 
-python CleanCSV.py $plfilename "tmp_pl.dat" $filtercol $filterval
+python CleanCSV.py .\data\$plfilename ".\data\tmp_pl.dat" $filtercol $filterval 
 
 Write-Host "Loading Provider Location data into SQL Server using bcp..."
-bcp NPPES.dbo.prov_loc IN tmp_pl.dat -f prov_loc_format.xml -e error.dat -m 10 -U $user -P $pswd -S $server
+bcp NPPES.dbo.prov_loc IN .\data\tmp_pl.dat -f prov_loc_format.xml -e error_provloc.dat -m 10 -S $server -T #-U $user -P $pswd 
 
