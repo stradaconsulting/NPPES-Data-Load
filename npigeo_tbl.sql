54d843c70608067514662f2c17adcfc7106b5914
Santiago Oleas <soleas@gmail.com>
2022-06-07 14:59:08 -0400

    Added new Staging table to npigeo process
    Identical table to npigeo created for staging purposes named npigeo_stage so that any created records may be placed in staging and moved to the main table. The staging table will ensure that no duplicates are sent over and to be able to incrementally add addresses to the main table.

--- npigeo_tbl.sql
+++ npigeo_tbl.sql
@@ -1,5 +1,3 @@
-USE NPPES
-
 -----------------------------
 --  npigeo
 -----------------------------
@@ -8,6 +6,23 @@ If OBJECT_ID('npigeo', 'U') IS NOT NULL
    DROP TABLE [npigeo]
 GO
 CREATE TABLE [dbo].[npigeo] (
+	[NPI] numeric not null,
+	[Matchflag] varchar(20) null,
+	[Matchtype] varchar(20) null,
+	[Longitude] float null,
+	[Latitude] float null
+);
+GO
+
+
+
+-----------------------------
+--  npigeo_stage
+-----------------------------
+If OBJECT_ID('npigeo_stage', 'U') IS NOT NULL
+   DROP TABLE [npigeo_stage]
+GO
+CREATE TABLE [dbo].[npigeo_stage] (
 	[NPI] numeric not null,
 	[Matchflag] varchar(20) null,
 	[Matchtype] varchar(20) null,
