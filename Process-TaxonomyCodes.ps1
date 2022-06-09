5b6266e0562860a2629e2a0db55e40229759fa47
Santiago Oleas <soleas@gmail.com>
2022-06-09 14:28:20 -0400

    Remove explicit database NPPES house
    Remove NPPES explicit database call and instead use $database from passwords.json and used in bcp command call as a parameter.

--- Process-TaxonomyCodes.ps1
+++ Process-TaxonomyCodes.ps1
@@ -6,7 +6,8 @@ $sqlparms = ./Get-SQLCredential.ps1
 $user     = $sqlparms['userid'] 
 $pswd     = $sqlparms['password']
 $server   = $sqlparms['server']
+$database = $sqlparms['database']
 
 python CleanCSV.py .\data\$txfilename ".\data\tmp_txcodes.dat"
 
-bcp NPPES.dbo.txcodes IN .\data\tmp_txcodes.dat -f tx_format.xml -e error_taxonomy.dat -m 10 -S $server -T #-U $user -P $pswd 
+bcp dbo.txcodes IN .\data\tmp_txcodes.dat -f tx_format.xml -e error_taxonomy.dat -m 10 -S $server -d $database -U $user -P $pswd #-T Either use -T or -U & -P depending on connection type
