49bd7fea5e8e79075aecdfdb1200ad807a5aebe3
Santiago Oleas <soleas@gmail.com>
2022-07-08 08:23:44 -0400

    Update Get-NPIData.ps1
    Added comments to ask the user to manually download the two additional data sources for Medicare DME and Medicare summary by Geography.

--- Get-NPIData.ps1
+++ Get-NPIData.ps1
@@ -30,3 +30,13 @@ Invoke-WebRequest -URI $txcodesURI -OutFile .\data\$txcodesFileName
 Write-Host "***Warning - You must download the Medicare data manually and place it into the .\data subfolder. The URI requires accepting terms and selecting the file"
 Write-Host "Navigate to this location and download the most recent single year:"
 Write-Host "https://data.cms.gov/provider-summary-by-type-of-service/medicare-physician-other-practitioners/medicare-physician-other-practitioners-by-provider-and-service"
+
+Write-Host "***Warning - The same must be done for the Durable Medical Equipment for Referring Provider"
+Write-Host "Navigate to this location and download the most recent single year and place into the .\data\ location"
+Write-Host "https://data.cms.gov/provider-summary-by-type-of-service/medicare-durable-medical-equipment-devices-supplies/medicare-durable-medical-equipment-devices-supplies-by-referring-provider-and-service"
+Write-Host "Be sure to extract the compressed file into the same .\data\ location"
+
+Write-Host "***Warning - The same must be done for the Medicare Physician & Other Practitioners - by Geography and Service"
+Write-Host "Navigate to this location and download the most recent single year and place into the .\data\ location"
+Write-Host "https://data.cms.gov/provider-summary-by-type-of-service/medicare-physician-other-practitioners/medicare-physician-other-practitioners-by-geography-and-service"
+Write-Host "Be sure to extract the compressed file into the same .\data\ location"
