4b86b555f76ce33da9d3ff673eecf5945cd72861
Santiago Oleas <soleas@gmail.com>
2022-07-08 08:15:00 -0400

    Added Medicare DME data into view for data ouput
    Added Medicare DME data into view for data output.

--- medicare_npi_latitude_longitude_view.sql
+++ medicare_npi_latitude_longitude_view.sql
@@ -10,9 +10,37 @@ If OBJECT_ID('medicare_npi_latitude_longitude', 'V') IS NOT NULL
 GO
 
 CREATE VIEW [dbo].[medicare_npi_latitude_longitude] AS
-   SELECT NPI                      npi
-        , Latitude                 latitude
-        , Longitude                longitude
-     FROM npigeo
+   SELECT n.NPI                      npi
+        , g.Latitude                 latitude
+        , g.Longitude                longitude
+        , t.classification           specialization
+        , CASE WHEN n.Entity_Type_Code = 1  --Non-Organizational
+               THEN trim(concat(n.Provider_Name_Prefix_Text, ' ',
+                                n.Provider_First_Name,' ',
+                                n.Provider_Last_Name,
+                                n.Provider_Name_Suffix_Text)) 
+               WHEN n.Entity_Type_Code = 2  --Organizational
+               THEN n.Provider_Org_Name
+          END                         provider_name
+        , g.MatchedAddress            address
+        , CASE WHEN n.Entity_Type_Code = 2 --Organizational
+               THEN trim(concat(n.Authorized_Official_First_Name, ' ',
+                                n.Authorized_Official_Last_Name, ', ',
+                                n.Authorized_Official_Title_or_Position))
+          END                         contact
+        , e.Endpoint                            email
+        , NULL                                  LinkedInProfile
+     FROM npidata                      n
+          INNER JOIN npi_taxonomy      nt
+                  ON nt.npi       = n.npi
+                 AND nt.Healthcare_Provider_Primary_Taxonomy_Switch = 'Y'
+          INNER JOIN txcodes           t
+                  ON t.Code       = nt.Healthcare_Provider_Taxonomy_Code
+          INNER JOIN medicare_npi_geo g
+                  ON g.Rndrng_NPI = n.npi
+                 AND g.Matchflag  = 'Match'
+          LEFT  JOIN npi_endpoint      e
+                  ON e.NPI        = n.NPI
+                 AND e.Endpoint   like '%@%'
 
 GO
--- medicare_npi_specialization_hcpcs_view.sql
+++ medicare_npi_specialization_hcpcs_view.sql
@@ -11,16 +11,46 @@ GO
 
 
 CREATE VIEW [dbo].[medicare_npi_specialization_hcpcs] AS
-   SELECT Rndrng_NPI               npi
-        , t.Classification         specialty
-        , HCPCS_Cd                 code
-        , round(Tot_Srvc,0)        count
-        , round(Avg_Sbmtd_Chrg,0)  value
+   SELECT m.Rndrng_NPI                    npi
+        , t.Classification                   specialty
+        , HCPCS_Cd                           code
+        , round(Tot_Srvc,0)                  count
+        , round(Avg_Sbmtd_Chrg,0)            value
+        , g.Latitude                         latitude
+        , g.Longitude                        longitude
+        , m.Avg_Mdcr_Alowd_Amt               avg_medicare_allowed
+        , m.Avg_Mdcr_Pymt_Amt                avg_medicare_billed
      FROM medicare_npi_hcpcs           m
+          INNER JOIN npidata           npi
+                  ON npi.npi      = m.Rndrng_NPI
           INNER JOIN npi_taxonomy      n
-                  ON n.npi      = m.Rndrng_NPI
+                  ON n.npi        = m.Rndrng_NPI
                  AND n.Healthcare_Provider_Primary_Taxonomy_Switch = 'Y'
           INNER JOIN txcodes           t
-                  ON t.Code     = n.Healthcare_Provider_Taxonomy_Code
+                  ON t.Code       = n.Healthcare_Provider_Taxonomy_Code
+          INNER JOIN medicare_npi_geo  g
+                  ON g.Rndrng_NPI = m.Rndrng_NPI
+                 AND Matchflag    = 'Match'
+UNION
+   SELECT m.Rfrg_NPI                      npi
+        , t.Classification                   specialty
+        , HCPCS_CD                           code
+        , round(Tot_Suplr_Srvcs,0)           count
+        , round(Avg_Suplr_Sbmtd_Chrg,0)      value
+        , g.Latitude                         latitude
+        , g.Longitude                        longitude
+        , m.Avg_Suplr_Mdcr_Alowd_Amt         avg_medicare_allowed
+        , m.Avg_Suplr_Mdcr_Pymt_Amt          avg_medicare_billed
+     FROM medicare_npi_hcpcs_dme       m
+          INNER JOIN npidata           npi
+                  ON npi.npi      = m.Rfrg_NPI
+          INNER JOIN npi_taxonomy      n
+                  ON n.npi        = m.Rfrg_NPI
+                 AND n.Healthcare_Provider_Primary_Taxonomy_Switch = 'Y'
+          INNER JOIN txcodes           t
+                  ON t.Code       = n.Healthcare_Provider_Taxonomy_Code
+          INNER JOIN medicare_npi_geo  g
+                  ON g.Rndrng_NPI = m.Rfrg_NPI
+                 AND Matchflag    = 'Match';
 GO
 
